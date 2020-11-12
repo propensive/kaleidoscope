7efd0ae03bd117ab40a29c4bae9b6ee9dd7c3d0c
Jon Pretty <jon.pretty@propensive.com>
2020-11-13 00:58:43 +0100

    Changes for 0.5.0 release

--- src/core/email.scala
+++ src/core/email.scala
@@ -1,6 +1,6 @@
 /*
 
-    Kaleidoscope, version 0.4.0. Copyright 2018-20 Jon Pretty, Propensive OÜ.
+    Kaleidoscope, version 0.5.0. Copyright 2018-20 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
