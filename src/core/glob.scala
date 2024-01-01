d613001c9e27340331f0881c27c51f5ac5c89bb8
propensive <propensive@users.noreply.github.com>
2024-01-01 18:21:50 +0000

    Apply automatic changes

--- src/core/glob.scala
+++ src/core/glob.scala
@@ -1,5 +1,5 @@
 /*
-    Kaleidoscope, version [unreleased]. Copyright 2023 Jon Pretty, Propensive OÜ.
+    Kaleidoscope, version [unreleased]. Copyright 2024 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
