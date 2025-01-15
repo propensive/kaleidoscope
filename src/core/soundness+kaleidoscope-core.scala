d33f5f08a7de6f98e203a60bea2f6a06b1334359
propensive <propensive@users.noreply.github.com>
2025-01-15 17:09:31 +0000

    Apply automatic changes

--- src/core/soundness+kaleidoscope-core.scala
+++ src/core/soundness+kaleidoscope-core.scala
@@ -1,5 +1,5 @@
 /*
-    Kaleidoscope, version 0.24.0. Copyright 2025 Jon Pretty, Propensive OÜ.
+    Kaleidoscope, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
